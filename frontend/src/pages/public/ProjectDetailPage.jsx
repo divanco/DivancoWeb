373312677719e4b79401c34a297bad3578c81b22
mlobeto <mercedeslobeto@gmail.com>
2025-09-09 16:03:36 -0300

    sintaxis ProjectDetail

--- frontend/src/pages/public/ProjectDetailPage.jsx
+++ frontend/src/pages/public/ProjectDetailPage.jsx
@@ -361,7 +361,7 @@ const VirtualTour = ({ kuulaUrl, t }) => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
               </svg>
             </div>
-          </button>
+          </a>
         </div>
         
         {/* Vista previa */}
